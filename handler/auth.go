56ef9b2770fbb7e63428420f584136dc90c88a2e
themmj <themmj@users.noreply.github.com>
2020-06-06 14:23:23 +0200

    Implement Add-To-Slack workflow

---
+++ handler/auth.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"net/http"
+	"sp-slack/logger"
+	"sp-slack/oauth"
+)
+
+func AddToSlack(w http.ResponseWriter, r *http.Request) {
+	url := oauth.GenerateButtonUrl()
+	w.Write([]byte(url))
+	logger.Info("button url returned")
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	// TODO check if state matches
+	code := r.URL.Query().Get("code")
+	logger.Infof("temp code: %+v", code)
+	response, err := oauth.GetOAuthV2AccessToken(code)
+	if err != nil {
+		logger.Error(err)
+		w.Write([]byte(err.Error()))
+	}
+	logger.Infof("access token for team %s with scopes %s : %s", response.Team.ID, response.Scope, response.AccessToken)
+	w.Write([]byte("Top"))
+}
