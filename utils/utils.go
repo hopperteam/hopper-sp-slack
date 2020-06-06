4ef912e8468c3956ed50050fe1c98c7d88870448
themmj <themmj@users.noreply.github.com>
2020-06-07 01:18:55 +0200

    Remove redundant body closing

--- utils/utils.go
+++ utils/utils.go
@@ -8,12 +8,10 @@ import (
 )
 
 func ParseJSONReq(r *http.Request, ref interface{}) error {
-	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(ref)
 }
 
 func ParseJSONRes(r *http.Response, ref interface{}) error {
-	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(ref)
 }
 
