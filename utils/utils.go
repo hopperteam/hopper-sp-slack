20ce1c5e3ba9eaf7cad6b03bcfd004b5f6284087
themmj <themmj@users.noreply.github.com>
2020-06-07 01:12:18 +0200

    Add abstraction to copy or move req body

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
+	"io/ioutil"
 )
 
 func ParseJSONReq(r *http.Request, ref interface{}) error {
@@ -13,4 +15,18 @@ func ParseJSONReq(r *http.Request, ref interface{}) error {
 func ParseJSONRes(r *http.Response, ref interface{}) error {
 	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(ref)
+}
+
+func MvReqBodyToStr(r *http.Request) (string, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	return string(body), err
+}
+
+func CpReqBodyToStr(r *http.Request) (string, error) {
+	body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        return string(body), err
+    }
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	return string(body), err
 }
